1b9862dbebcf8106cdce0dcea43fbbb085b870fc
PiusAremieye <ebi44ril@gmail.com>
2020-03-17 18:51:33 +0100

    add react build files

---
+++ JavaApi/src/main/resources/static/precache-manifest.6880dbaec11d88196df31d03de3ecd6f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c3fdc0f1bb5f21cc47e8444073cfcb0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60567663292eff037fa1",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "21054fe741612dfc4e94",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "60567663292eff037fa1",
+    "url": "/static/js/2.6314e562.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.6314e562.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21054fe741612dfc4e94",
+    "url": "/static/js/main.42d75b63.chunk.js"
+  },
+  {
+    "revision": "68b72371ddd4370bc360",
+    "url": "/static/js/runtime-main.8f3f3ca0.js"
+  }
+]);
